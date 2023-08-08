0fcc7ee5b8927c72a283cba07221df569f4a2944
NriotHrreion <nriot233@outlook.com>
2023-08-08 21:16:49 +0800

    chore: Upgrade to 1.2.1

--- src/global.tsx
+++ src/global.tsx
@@ -2,7 +2,7 @@ import Utils from "./utils/Utils";
 import Emitter from "./utils/Emitter";
 import { MathFunction, Shortcut } from "./types";
 
-export const version = "1.2.0";
+export const version = "1.2.1";
 export const errorText = "\\text{Error}";
 
 export const functions: Map<string, MathFunction> = new Map([
