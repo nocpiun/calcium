694fa977a0edffa2c7b8cf1d9afdfa66ef418c37
NriotHrreion <nriot233@outlook.com>
2023-09-01 14:08:44 +0800

    fix: Cannot input hex numbers `A~F` by keyboard

--- src/utils/Utils.tsx
+++ src/utils/Utils.tsx
@@ -75,6 +75,7 @@ export default class Utils {
         const allowed = [
             "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
             "A", "B", "C", "D", "E", "F",
+            "a", "b", "c", "d", "e", "f",
             "+", "-", "*", "/", "<", ">", "%",
             "Enter", "Backspace", "ArrowLeft", "ArrowRight"
         ];
