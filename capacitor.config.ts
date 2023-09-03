37bc9523b7c3c219f271830ae96c4bde4d50111e
NriotHrreion <nriot233@outlook.com>
2023-09-03 14:04:39 +0800

    ci: Capacitor apk building

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+    appId: "net.nocpiun.calcium.android",
+    appName: "Calcium 计算器",
+    webDir: "build",
+}
+
+export default config;
