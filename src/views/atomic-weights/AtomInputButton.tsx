7dca3c47bed694039c727ba254b67836799ce4ae
NriotHrreion <nriot233@outlook.com>
2024-04-04 21:11:14 +0800

    feat(atomic-weights): Display element number and mass in the element buttons

--- src/views/atomic-weights/AtomInputButton.tsx
+++ src/views/atomic-weights/AtomInputButton.tsx
@@ -2,6 +2,7 @@ import React, { useMemo } from "react";
 import { elements } from "chemical-elements";
 
 import InputButton from "@/components/InputButton";
+import { getAtomicWeight } from "@/views/atomic-weights/AtomicWeightsPage";
 
 import Emitter from "@/utils/Emitter";
 
@@ -11,6 +12,7 @@ interface AtomInputButtonProps {
 
 const AtomInputButton: React.FC<AtomInputButtonProps> = (props) => {
     const element = useMemo(() => elements[props.number - 1], [props.number]);
+    const mass = useMemo(() => getAtomicWeight(props.number), [props.number]);
 
     const handleClick = () => {
         new Emitter().emit("atom-input", props.number);
@@ -18,7 +20,7 @@ const AtomInputButton: React.FC<AtomInputButtonProps> = (props) => {
 
     return (
         <InputButton
-            symbol={"\\text{"+ element.symbol +"}"}
+            symbol={(element.mass ? "^{"+ mass +"}" : "") +"_{"+ element.number +"}\\text{"+ element.symbol +"}"}
             inputValue={element.symbol}
             grow={1}
             title={element.name}
